8f514d2074f732b1e5680854e40197c50db9732b
Andrei Coman <andrei.coman@3pillarglobal.com>
2020-07-30 15:45:53 +0300

    Fix DB

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'HOST': os.environ.get('DB_HOST'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'CONN_MAX_AGE': int(os.environ.get('CONN_MAX_AGE', '15'))
     }
 }
 
