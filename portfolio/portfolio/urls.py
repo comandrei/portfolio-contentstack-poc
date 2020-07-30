55b445e7804a3c6efd8a15f0ef1d9812be9cf25c
Andrei Coman <andrei.coman@3pillarglobal.com>
2020-07-30 15:46:07 +0300

    Minor styling of base page

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.contrib.staticfiles import views
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web.urls'))
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', views.serve),
+    ]
